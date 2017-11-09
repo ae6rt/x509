e5402ebff9c5922be6fdae69578f2654e35d587c
Mark Petrovic <mpetrovic@paypal.com>
2017-11-09 12:47:31 -0800

    [master] default certificate input file to standard input

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 
 // Print useful certificate fields without openssl-x509 fuss.
 func main() {
-	certFile := flag.String("cert", "", "File containing one PEM encoded X.509 certificate.")
+	certFile := flag.String("cert", "-", "File containing one PEM encoded X.509 certificate.  Defaults to standard input.")
 	flag.Parse()
 
 	var f io.Reader
